712558650620a12b66b72a76a4d48db705e28c98
Zac Robinson <zachary.robinson@sky.uk>
2018-08-29 22:46:46 +0100

    Complete offer work

--- src/basket.js
+++ src/basket.js
@@ -8,15 +8,34 @@ export class Basket {
     const itemIndex = this.items.indexOf(item);
     if (itemIndex !== -1) {
       this.items[itemIndex].quantity++;
-    }
-    else {
+    } else {
       this.items.push(item);
     }
   }
 
   total() {
     let totalPrice = 0.0;
-    this.items.forEach(item => totalPrice += item.price);
+    this.items.forEach(item => {
+      totalPrice += this.applyOffer(item, this.pricingRules)
+    });
     return totalPrice.toFixed(2);
+  }
+
+  applyOffer(item, pricingRules) {
+    const currentPrice = item.price * item.quantity;
+    let offerPrice = 0;
+    if (pricingRules[item.productCode] === 'bogof') {
+      if (item.quantity % 2 === 0) {
+        offerPrice = currentPrice / 2;
+      } else {
+        offerPrice = ((currentPrice - item.price) / 2) + item.price;
+      }
+    } else if (pricingRules[item.productCode] === 'bulk' && item.quantity >= 3) {
+      const SR1Price = 4.5;
+      offerPrice = SR1Price * item.quantity;
+    } else {
+      offerPrice = currentPrice;
+    }
+    return offerPrice;
   }
 }
--- test/basket.test.js
+++ test/basket.test.js
@@ -15,7 +15,7 @@ const storeItems = [
     quantity: 1
   },
   {
-    productCode: 'CR1',
+    productCode: 'CF1',
     productName: 'Coffee',
     price: 11.23,
     quantity: 1
@@ -23,8 +23,8 @@ const storeItems = [
 ]
 
 const pricingRules = {
-  bogof: 'FR1',
-  bulkBuy: 'SR1'
+  FR1: 'bogof',
+  SR1: 'bulk'
 }
 
 describe('Basket', () => {
@@ -105,13 +105,263 @@ describe('Basket', () => {
     it('should contain pricing rules when they are supplied', () => {
       const basket = new Basket(pricingRules);
       const expectedPriceRules = {
-        bogof: 'FR1',
-        bulkBuy: 'SR1'
+        FR1: 'bogof',
+        SR1: 'bulk'
       };
 
       assert.isObject(basket.pricingRules);
       expect(basket.pricingRules).to.deep.equal(expectedPriceRules);
     });
-  })
+
+    it('should calculate correct total if you add 1 FR1 items to the basket with BOGOF', () => {
+      const basket = new Basket(pricingRules);
+      const FR1 = {
+        productCode: 'FR1',
+        productName: 'Fruit Tea',
+        price: 3.11,
+        quantity: 1
+      };
+
+      basket.add(FR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '3.11')
+    });
+
+    it('should calculate correct total if you add 2 FR1 products to the basket with BOGOF', () => {
+      const basket = new Basket(pricingRules);
+      const FR1 = {
+        productCode: 'FR1',
+        productName: 'Fruit Tea',
+        price: 3.11,
+        quantity: 1
+      };
+
+      basket.add(FR1);
+      basket.add(FR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '3.11')
+    });
+
+    it('should calculate correct total if you add 3 FR1 items to the basket with BOGOF', () => {
+      const basket = new Basket(pricingRules);
+      const FR1 = {
+        productCode: 'FR1',
+        productName: 'Fruit Tea',
+        price: 3.11,
+        quantity: 1
+      };
+
+      basket.add(FR1);
+      basket.add(FR1);
+      basket.add(FR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '6.22')
+    });
+
+    it('should calculate correct total if you add 4 FR1 items to the basket with BOGOF', () => {
+      const basket = new Basket(pricingRules);
+      const FR1 = {
+        productCode: 'FR1',
+        productName: 'Fruit Tea',
+        price: 3.11,
+        quantity: 1
+      };
+
+      basket.add(FR1);
+      basket.add(FR1);
+      basket.add(FR1);
+      basket.add(FR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '6.22')
+    });
+
+    it('should calculate correct total if you add 1 SR1 items to the basket with bulk offer', () => {
+      const basket = new Basket(pricingRules);
+      const SR1 = {
+        productCode: 'SR1',
+        productName: 'Strawberries',
+        price: 5.00,
+        quantity: 1
+      };
+
+      basket.add(SR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '5.00')
+    });
+
+    it('should calculate correct total if you add 2 SR1 products to the basket with bulk offer', () => {
+      const basket = new Basket(pricingRules);
+      const SR1 = {
+        productCode: 'SR1',
+        productName: 'Strawberries',
+        price: 5.00,
+        quantity: 1
+      };
+
+      basket.add(SR1);
+      basket.add(SR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '10.00')
+    });
+
+    it('should calculate correct total if you add 3 SR1 items to the basket with bulk offer', () => {
+      const basket = new Basket(pricingRules);
+      const SR1 = {
+        productCode: 'SR1',
+        productName: 'Strawberries',
+        price: 5.00,
+        quantity: 1
+      };
+
+      basket.add(SR1);
+      basket.add(SR1);
+      basket.add(SR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '13.50')
+    });
+
+    it('should calculate correct total if you add 4 SR1 items to the basket with bulk offer', () => {
+      const basket = new Basket(pricingRules);
+      const SR1 = {
+        productCode: 'SR1',
+        productName: 'Strawberries',
+        price: 5.00,
+        quantity: 1
+      };
+
+      basket.add(SR1);
+      basket.add(SR1);
+      basket.add(SR1);
+      basket.add(SR1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '18.00')
+    });
+
+    it('should calculate correct total if you add 1 CF1 items to the basket with no offer', () => {
+      const basket = new Basket(pricingRules);
+      const CF1 = {
+        productCode: 'CF1',
+        productName: 'Coffee',
+        price: 11.23,
+        quantity: 1
+      };
+
+      basket.add(CF1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '11.23')
+    });
+
+    it('should calculate correct total if you add 2 CF1 products to the basket with no offer', () => {
+      const basket = new Basket(pricingRules);
+      const CF1 = {
+        productCode: 'CF1',
+        productName: 'Coffee',
+        price: 11.23,
+        quantity: 1
+      };
+
+      basket.add(CF1);
+      basket.add(CF1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '22.46')
+    });
+
+    it('should calculate correct total if you add 3 CF1 products to the basket with no offer', () => {
+      const basket = new Basket(pricingRules);
+      const CF1 = {
+        productCode: 'CF1',
+        productName: 'Coffee',
+        price: 11.23,
+        quantity: 1
+      };
+
+      basket.add(CF1);
+      basket.add(CF1);
+      basket.add(CF1);
+
+      assert.strictEqual(basket.items.length, 1);
+      assert.strictEqual(basket.total(), '33.69')
+    });
+  });
+
+  describe('Using test data', () => {
+    it('should show correct price for basket: FR1, SR1, FR1, CF1', () => {
+      const basket = new Basket(pricingRules);
+      const CF1 = {
+        productCode: 'CF1',
+        productName: 'Coffee',
+        price: 11.23,
+        quantity: 1
+      };
+      const SR1 = {
+        productCode: 'SR1',
+        productName: 'Strawberries',
+        price: 5.00,
+        quantity: 1
+      };
+      const FR1 = {
+        productCode: 'FR1',
+        productName: 'Fruit Tea',
+        price: 3.11,
+        quantity: 1
+      };
+
+      basket.add(FR1);
+      basket.add(SR1);
+      basket.add(FR1);
+      basket.add(CF1);
+
+      assert.strictEqual(basket.total(), '19.34');
+    });
+
+    it('should show correct price for basket: FR1, FR1', () => {
+      const basket = new Basket(pricingRules);
+      const FR1 = {
+        productCode: 'FR1',
+        productName: 'Fruit Tea',
+        price: 3.11,
+        quantity: 1
+      };
+
+      basket.add(FR1);
+      basket.add(FR1);
+
+      assert.strictEqual(basket.total(), '3.11');
+    });
+
+    it('should show correct price for basket: SR1, SR1, FR1, SR1', () => {
+      const basket = new Basket(pricingRules);
+      const FR1 = {
+        productCode: 'FR1',
+        productName: 'Fruit Tea',
+        price: 3.11,
+        quantity: 1
+      };
+
+      const SR1 = {
+        productCode: 'SR1',
+        productName: 'Strawberries',
+        price: 5.00,
+        quantity: 1
+      };
+
+      basket.add(SR1);
+      basket.add(SR1);
+      basket.add(FR1);
+      basket.add(SR1);
+
+      assert.strictEqual(basket.total(), '16.61');
+    });
 })
+});
 
